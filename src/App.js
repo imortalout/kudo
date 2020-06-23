dc215f8bf10a41c5f5f17e19071bbf2a8bdb57dd
imortalout <imortalout@gmail.com>
2020-06-22 23:35:33 -0300

    bug fixed

--- src/App.js
+++ src/App.js
@@ -48,7 +48,40 @@ export default class App extends React.Component {
     yellow: 0,
     feed: false,
     openFeed: false,
-    imageUser: 'https://firebasestorage.googleapis.com/v0/b/kudo-344d6.appspot.com/o/user.png?alt=media&token=cd87a326-b63e-476d-bb61-ec255cbcae52'
+    imageUser: 'https://firebasestorage.googleapis.com/v0/b/kudo-344d6.appspot.com/o/user.png?alt=media&token=cd87a326-b63e-476d-bb61-ec255cbcae52',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
   }
 
   constructor(props) {
@@ -349,9 +382,11 @@ export default class App extends React.Component {
     })
   }
 
+
+
   render() {
 
-    var { votes, nameUser, imageUser, blue, pink, yellow, openFeed, feed, kudos } = this.state
+    var { votes, nameUser, imageUser, blue, pink, yellow, openFeed, feed, kudos, data, options } = this.state
 
     var refThis = this
 
